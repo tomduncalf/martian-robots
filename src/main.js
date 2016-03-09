0c271d37fd17998431f00c75fb814de1c94f229c
Tom Duncalf <tom@tomduncalf.com>
2016-03-09 19:30:58 +0000

    Progress up to robot getting lost

--- src/main.js
+++ src/main.js
@@ -11,36 +11,116 @@ function processInput(input) {
   }
 
   const [ worldW, worldH ] = lines[0].split(' ')
-  let world = createWorld(worldW, worldH)
-
-  let outputs = []
   const robotInstructions = _.chunk(2, lines.slice(1))
 
-  robotInstructions.map(instruction => {
-    const [ startPos, sequence ] = instruction
+  const initialState = {
+    world: createWorld(parseInt(worldW, 10), parseInt(worldH, 10)),
+    output: []
+  }
 
-    const result = processRobotSequence(world, startPos, sequence)
-    world = result[0]
-    outputs.push(result[1])
-  })
+  const finalState = robotInstructions.reduce(
+    (accState, instruction) => {
+      const [ startPos, sequence ] = instruction
+      console.log(`startPos ${startPos}, sequence ${sequence}`)
 
-  return outputs.join('\n')
+      const result = processRobotSequence(accState.world, startPos, sequence)
+
+      return {
+        world: result['world'],
+        output: accState.output.concat(result['output'])
+      }
+    },
+    initialState
+  )
+
+  return finalState.output.join('\n')
+}
+
+// createWorld :: Number -> Number -> [Boolean]
+// Create a 2-dimensional array of booleans representing the world and whether a robot
+// has been lost at a given position
+function createWorld(width, height) {
+  if (!(width && height)) {
+    throw new Error('No world dimensions found')
+  }
+
+  if (width > 50 || height > 50) {
+    throw new Error('Maximum world dimensions exceeded')
+  }
+
+  const world = _.range(0, width).map(() => _.range(0, height).map(() => false))
+  return world
 }
 
 // processRobotSequence :: World -> String -> String -> [World, String]
 // Take a start position and sequence of instructions for a robot and return the state of the world
 // after the robots has navigated it, and the final position outpuy for the robot
 function processRobotSequence(world, startPos, sequence) {
-  return [ world, sequence ]
-}
+  const splitStartPos = startPos.split(' ')
+  const splitSequence = sequence ? sequence.split('') : []
 
-// createWorld :: Number -> Number -> [Boolean]
-function createWorld(width, height) {
-  if (!width && height) {
-    throw new Error('No world dimensions found')
+  if (splitStartPos.length !== 3) {
+    throw new Error(`Invalid start position supplied: ${startPos}`)
   }
 
-  return []
+  const initialState = {
+    position: { x: parseInt(splitStartPos[0], 10), y: parseInt(splitStartPos[1], 10) },
+    direction: splitStartPos[2],
+    lost: false
+  }
+
+  const finalState = splitSequence.reduce(
+    (accState, step) => {
+      return processStep(accState, step)
+    },
+    initialState
+  )
+
+  return {
+    world: world,
+    output: `${finalState.position.x} ${finalState.position.y} ${finalState.direction}`
+  }
+}
+
+function processStep(state, step) {
+  const newState = Object.assign({}, state)
+
+  switch (step) {
+    case 'F':
+      newState.position = getRelativePosition(state.position, state.direction, 1)
+      break
+    case 'R':
+      newState.direction = getRelativeDirection(state.direction, 1)
+      break;
+    case 'L':
+      newState.direction = getRelativeDirection(state.direction, -1)
+      break;
+  }
+
+  return newState
+}
+
+function getRelativePosition(position, direction, delta) {
+  switch (direction) {
+    case 'N':
+      return { x: position.x, y: position.y + delta }
+    case 'E':
+      return { x: position.x + delta, y: position.y }
+    case 'S':
+      return { x: position.x, y: position.y - delta }
+    case 'W':
+      return { x: position.x - delta, y: position.y }
+  }
+}
+
+function getRelativeDirection(direction, delta) {
+  const directions = ['N', 'E', 'S', 'W']
+  return directions[mod((directions.indexOf(direction) + delta), directions.length)];
+}
+
+// JS modulo doesn't behave for negatives: http://stackoverflow.com/questions/4467539/javascript-modulo-not-behaving
+function mod(n, m) {
+    return ((n % m) + m) % m;
 }
 
 module.exports = processInput
--- src/main.spec.js
+++ src/main.spec.js
@@ -19,25 +19,125 @@ describe('martian robots', () => {
   describe('zero length sequence', () => {
     it('should not move a robot from 0,0 N', () => {
       const input = generateSequence(10, 10, [['0 0 N']])
-      expect(processInputSequence(input)).to.equal('00N')
+      expect(processInputSequence(input)).to.equal('0 0 N')
     })
 
     it('should not move a robot from 1,1 N', () => {
       const input = generateSequence(10, 10, [['1 1 N']])
-      expect(processInputSequence(input)).to.equal('11N')
+      expect(processInputSequence(input)).to.equal('1 1 N')
     })
 
     it('should not move a robot from 1,1 E', () => {
       const input = generateSequence(10, 10, [['1 1 E']])
-      expect(processInputSequence(input)).to.equal('11E')
+      expect(processInputSequence(input)).to.equal('1 1 E')
     })
   })
 
-  describe('single robot, single movement forward', () => {
-    it('should move a robot from 0,0 N to 1,0 N', () => {
-      const input = generateSequence(10, 10, [['0 0 N', 'F']])
-      expect(processInputSequence(input)).to.equal('10N')
+  describe('single robot', () => {
+    describe('simple sequences', () => {
+      describe('"F" sequence', () => {
+        it('should move a robot from 0,0 N to 0,1 N', () => {
+          const input = generateSequence(10, 10, [['0 0 N', 'F']])
+          expect(processInputSequence(input)).to.equal('0 1 N')
+        })
+
+        it('should move a robot from 0,0 E to 1,0 E', () => {
+          const input = generateSequence(10, 10, [['0 0 E', 'F']])
+          expect(processInputSequence(input)).to.equal('1 0 E')
+        })
+
+        it('should move a robot from 1,1 S to 1,0 S', () => {
+          const input = generateSequence(10, 10, [['1 1 S', 'F']])
+          expect(processInputSequence(input)).to.equal('1 0 S')
+        })
+
+        it('should move a robot from 1,1 W to 0,1 W', () => {
+          const input = generateSequence(10, 10, [['1 1 W', 'F']])
+          expect(processInputSequence(input)).to.equal('0 1 W')
+        })
+      })
+
+      describe('"FFFFF" sequence', () => {
+        it('should move a robot from 0,0 N to 0,5 N', () => {
+          const input = generateSequence(10, 10, [['0 0 N', 'FFFFF']])
+          expect(processInputSequence(input)).to.equal('0 5 N')
+        })
+      })
+
+      describe('"R" sequence', () => {
+        it('should move a robot from 0,0 N to 0,0 E', () => {
+          const input = generateSequence(10, 10, [['0 0 N', 'R']])
+          expect(processInputSequence(input)).to.equal('0 0 E')
+        })
+
+        it('should move a robot from 0,0 E to 0,0 S', () => {
+          const input = generateSequence(10, 10, [['0 0 E', 'R']])
+          expect(processInputSequence(input)).to.equal('0 0 S')
+        })
+
+        it('should move a robot from 0,0 S to 0,0 W', () => {
+          const input = generateSequence(10, 10, [['0 0 S', 'R']])
+          expect(processInputSequence(input)).to.equal('0 0 W')
+        })
+
+        it('should move a robot from 0,0 W to 0,0 N', () => {
+          const input = generateSequence(10, 10, [['0 0 W', 'R']])
+          expect(processInputSequence(input)).to.equal('0 0 N')
+        })
+      })
+
+      describe('"L" sequence', () => {
+        it('should move a robot from 0,0 N to 0,0 W', () => {
+          const input = generateSequence(10, 10, [['0 0 N', 'L']])
+          expect(processInputSequence(input)).to.equal('0 0 W')
+        })
+
+        it('should move a robot from 0,0 E to 0,0 N', () => {
+          const input = generateSequence(10, 10, [['0 0 E', 'L']])
+          expect(processInputSequence(input)).to.equal('0 0 N')
+        })
+
+        it('should move a robot from 0,0 S to 0,0 E', () => {
+          const input = generateSequence(10, 10, [['0 0 S', 'L']])
+          expect(processInputSequence(input)).to.equal('0 0 E')
+        })
+
+        it('should move a robot from 0,0 W to 0,0 S', () => {
+          const input = generateSequence(10, 10, [['0 0 W', 'L']])
+          expect(processInputSequence(input)).to.equal('0 0 S')
+        })
+      })
+
+      describe('"RR" sequence', () => {
+        it('should move a robot from 0,0 N to 0,0 S', () => {
+          const input = generateSequence(10, 10, [['0 0 N', 'RR']])
+          expect(processInputSequence(input)).to.equal('0 0 S')
+        })
+      })
+
+      describe('"LL" sequence', () => {
+        it('should move a robot from 0,0 N to 0,0 S', () => {
+          const input = generateSequence(10, 10, [['0 0 N', 'LL']])
+          expect(processInputSequence(input)).to.equal('0 0 S')
+        })
+      })
     })
+
+    describe('mixed sequences', () => {
+      it('"FFRFFLFF" should move a robot from 0,0N to 2,4N', () => {
+        const input = generateSequence(10, 10, [['0 0 N', 'FFRFFLFF']])
+        expect(processInputSequence(input)).to.equal('2 4 N')
+      })
+    })
+
+    describe('robot is lost when moving off the grid', () => {
+      it('is lost when moving off the north of the grid', () => {
+        const input = generateSequence(10, 10, [['0 10 N', 'F']])
+        expect(processInputSequence(input)).to.equal('0 10 LOST')
+      })
+    })
+
+
   })
 
   describe('limits', () => {
